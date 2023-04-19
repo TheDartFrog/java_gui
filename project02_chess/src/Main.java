f57f3904cbd07588902c574481612303a5820846
TheDartFrog <30552512+TheDartFrog@users.noreply.github.com>
2023-04-19 12:40:33 +0200

    AWT should be forbidden by law

--- project02_chess/src/Main.java
+++ project02_chess/src/Main.java
@@ -4,21 +4,15 @@ import java.io.*;
 import java.util.*;
 import javax.imageio.ImageIO;
 
-
-//////////////////////////////////////////////////
 public class Main extends Frame {
-
     //declare textures
     public Image chessboardImage;
-    public Image pawnImage;
-    public Image rookImage;
-    public Image horseyImage;
-    public Image bishopImage;
-    public Image queenImage;
-    public Image kingImage;
-    public Image emptyImage;
-    public Image targetImage;
-    //////
+
+    public Button[][] chessboardButtons;
+
+    public char[][] chessboardPosition = new char[8][8];
+
+    public Font chessFont = new Font( "MS Gothic", Font.PLAIN, 24);
 
 
 
@@ -29,7 +23,26 @@ public class Main extends Frame {
         setResizable(false);
 
 
+        //Create the chessboardButtons grid
+        chessboardButtons = new Button[9][9];
+        //////////////////////
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                chessboardButtons[i][j] = new Button();
 
+                chessboardButtons[i][j].setBounds(i * 128 + 8, j * 128 + 31, 128, 128);
+                if(i%2!=0 && j%2==0)
+                {
+                    chessboardButtons[i][j].setBackground(new Color(245, 0, 4)); // make the button black
+                }
+                else if(i%2==0 && j%2!=0)
+                {
+                    chessboardButtons[i][j].setBackground(new Color(245, 4, 4)); // make the button black
+                }
+                add(chessboardButtons[i][j]);
+                chessboardButtons[i][j].setFont(chessFont);
+            }
+        }
 
         ////////// make the X button work
         this.addWindowListener(new WindowAdapter() {
@@ -44,49 +57,63 @@ public class Main extends Frame {
     }
     ////////////////////////////
 
-
-
     ////////////////////////////
     public static void main(String[] args) throws IOException {
         Main frame = new Main();
         frame.loadImages(); //loading textures
-        frame.setSize(1024+16, 1024+8+31); //accounting for window margins
+
+        frame.setSize(1024 + 16, 1024 + 8 + 31); //accounting for window margins
         frame.setVisible(true);
+
+        frame.initializeBoard();
+        frame.paint(frame.getGraphics());
     }
     ////////////////////////////
-
-
 
     ////////////////////////////
     @Override
     public void paint(Graphics g) {
         g.drawImage(chessboardImage, 8, 31, null); //draw the chessboard
-
     }
     ////////////////////////////
-
-
 
     ////////////////////////////
     public void loadImages() throws IOException {
         chessboardImage = ImageIO.read(new File("chessboard.png")); //read background/chessboard image
+    }
 
-        pawnImage = ImageIO.read(new File("pawn.png")); //read pieces images
-        rookImage = ImageIO.read(new File("rook.png"));
-        horseyImage = ImageIO.read(new File("hors.png"));
-        bishopImage = ImageIO.read(new File("bish.png"));
-        queenImage = ImageIO.read(new File("quen.png"));
-        kingImage = ImageIO.read(new File("king.png"));
+    public void updateChessboard()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+             chessboardButtons[j][i].setLabel(String.valueOf(chessboardPosition[i][j]));
 
-        emptyImage = ImageIO.read(new File("empty.png")); //read empty slot image for easy layout handling
+             System.out.println(i+"\n"+j);
+             chessboardButtons[i][j].setFont(chessFont);
 
-        targetImage = ImageIO.read(new File("target.png")); //read 'possible move' highlight image
+            }
+        }
+        chessboardButtons[7][7].setLabel(String.valueOf(chessboardPosition[7][7]));
+    }
+
+    public void initializeBoard()
+    {
+
+        chessboardPosition[0] = new char[]{'♜','♞','♝','♛','♚','♝','♞','♜'};
+        chessboardPosition[1] = new char[]{'♟','♟','♟','♟','♟','♟','♟', '♟'};
+        chessboardPosition[2] = new char[]{' ',' ',' ',' ',' ',' ',' ',' '};
+        chessboardPosition[3] = new char[]{' ','A','M','O','G','U','S',' '};
+        chessboardPosition[4] = new char[]{' ',' ',' ',' ',' ',' ',' ',' '};
+        chessboardPosition[5] = new char[]{' ',' ',' ',' ',' ',' ',' ',' '};
+        chessboardPosition[6] = new char[]{'♙','♙','♙','♙','♙','♙','♙','♙'};
+        chessboardPosition[7] = new char[]{'♖','♘','♗','♕','♔','♗','♘','♖', ' '};
 
 
+
+        updateChessboard();
 
     }
     ////////////////////////////
-
-
 }
-//////////////////////////////////////////////////
